363d4ac80866905122c67c9f0281838c61528851
askepen <aske@vhg26.dk>
2020-10-22 10:23:23 +0200

    Deserialization supports optional fields

--- src/story_parser.rs
+++ src/story_parser.rs
@@ -1,93 +1,55 @@
 use std::fs::File;
 use std::path::Path;
 
-#[derive(Deserialize,Debug)]
+#[derive(Default, Deserialize,Debug)]
+#[serde(rename="idPkg:Story")]
+#[serde(rename_all="PascalCase")]
+pub struct StoryWrapper {
+    story: Option<Story>
+}
+
+#[derive(Default, Deserialize,Debug)]
+#[serde(rename_all="PascalCase")]
 pub struct Story {
-    // #[serde(rename="Self")]
-    // id: String,
-    #[serde(default)]
-    UserText: bool,
-
-    #[serde(default)]
-    StoryTitle: String,
-    
-    #[serde(default)]
-    StoryPreference: StoryPreference,
-
-    #[serde(default)]
-    InCopyExportOption: InCopyExportOption,
-
-    #[serde(default)]
+    //#[serde(rename="Self")]
+    //id: String,
+    user_text: Option<bool>,
+    story_title: Option<String>,
     #[serde(rename="ParagraphStyleRange")]
-    ParagraphStyleRanges: Vec<ParagraphStyleRange>
+    paragraph_style_ranges: Option<Vec<ParagraphStyleRange>>
 }
 
-#[derive(Deserialize,Debug)]
-pub struct StoryPreference {
-    #[serde(default)]
-    OpticalMarginAlignment: bool,
-}
-
-impl Default for StoryPreference {
-    fn default() -> Self{
-        StoryPreference {
-            OpticalMarginAlignment: true
-        }
-    }
-}
-
-#[derive(Deserialize,Debug)]
-pub struct InCopyExportOption {
-    #[serde(default)]
-    IncludeGraphicProxies: bool,
-}
-
-impl Default for InCopyExportOption {
-    fn default() -> Self{
-        InCopyExportOption {
-            IncludeGraphicProxies: false
-        }
-    }
-}
-
-#[derive(Deserialize,Debug)]
+#[derive(Default,Deserialize,Debug)]
+#[serde(rename_all="PascalCase")]
 pub struct ParagraphStyleRange {
-    #[serde(default)]
-    AppliedParagraphStyle: String,
-
-    #[serde(default)]
-    Justification: String,
-    
-    #[serde(default)]
+    applied_paragraph_style: Option<String>,
+    justification: Option<String>,
     #[serde(rename="CharacterStyleRange")]
-    CharacterStyleRanges: Vec<CharacterStyleRange>
+    character_style_ranges: Option<Vec<CharacterStyleRange>>
 }
 
-#[derive(Deserialize,Debug)]
+#[derive(Default,Deserialize,Debug)]
+#[serde(rename_all="PascalCase")]
 pub struct CharacterStyleRange {
-    #[serde(default)]
-    AppliedCharacterStyle: String,
-
-    #[serde(default)]
-    FillColor: String,
-
-    #[serde(default)]
-    FontStyle: String, 
-
-    #[serde(default)]
-    PointSize: String,
+    applied_character_style: Option<String>,
+    fill_color: Option<String>,
+    font_style: Option<String>, 
+    point_size: Option<String>,
     #[serde(rename="Content")]
-    Contents: Vec<Content>
+    contents: Option<Vec<Content>>
 }
 
-#[derive(Deserialize,Debug)]
+#[derive(Default,Deserialize,Debug)]
+#[serde(rename_all="PascalCase")]
 pub struct Content {
     #[serde(rename="$value")]
     text: String,
 }
 
-pub fn parse_story_from_path(path: &Path) -> Story {
-    let test_xml = r##"<Story><ParagraphStyleRange AppliedParagraphStyle="ParagraphStyle\kNormalParagraphStyle"> <CharacterStyleRange AppliedCharacterStyle="CharacterStyle\k[No character style]"
+pub fn parse_story_from_path(path: &Path) -> StoryWrapper {
+    let test_xml = r##"
+    <idPkg:Story xmlns:idPkg="http://ns.adobe.com/AdobeInDesign/idml/1.0/packaging" DOMVersion="15.0">
+    <Story><ParagraphStyleRange AppliedParagraphStyle="ParagraphStyle\kNormalParagraphStyle"> <CharacterStyleRange AppliedCharacterStyle="CharacterStyle\k[No character style]"
     FillColor="Color\cRed" FontStyle="Italic" PointSize="24"> <Content>ABC</Content>
     <br/>
     </CharacterStyleRange>
@@ -96,9 +58,12 @@ pub fn parse_story_from_path(path: &Path) -> Story {
     <CharacterStyleRange AppliedCharacterStyle="CharacterStyle\k[No character style]">
         <Content>ABC</Content>
      </CharacterStyleRange>
-    </ParagraphStyleRange></Story>"##;
+    </ParagraphStyleRange></Story>
+    </idPkg:Story>"##;
 
-     let story = serde_xml_rs::from_str(std::fs::read_to_string(path).unwrap().as_str()).unwrap();
+    println!("{}", std::fs::read_to_string(path).unwrap().as_str());
+
+    let story = serde_xml_rs::from_str(std::fs::read_to_string(path).unwrap().as_str()).unwrap();
     //let story = serde_xml_rs::from_str(test_xml).unwrap();
 
     story
