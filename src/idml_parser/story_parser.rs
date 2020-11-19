aa8db7ece9474404837833053f03466f37a5f87d
askepen <aske@vhg26.dk>
2020-11-19 22:15:00 +0100

    Finally got story content working

--- src/idml_parser/story_parser.rs
+++ src/idml_parser/story_parser.rs
@@ -38,10 +38,8 @@ pub struct CharacterStyleRange {
     font_style: Option<String>, 
     point_size: Option<String>,
     properties: Option<Properties>,
-    // #[serde(deserialize_with = "deserialize_content", flatten)]
-    // #[serde(rename="Content", alias="Br")]
-    // #[serde(rename="Content")]
-    contents: Option<Vec<Content>>,
+    #[serde(rename="$value")]
+    contents: Option<Vec<StoryContent>>,
 }
 
 #[derive(Default,Deserialize,Debug)]
@@ -50,51 +48,24 @@ struct Properties {
     applied_font: Option<String> 
 }
 
-// #[derive(Deserialize,Debug)]
-// enum ContentOrLineBreak {
-//     Content(String),
-//     Br(String)
-// }
+#[derive(Deserialize,Debug)]
+enum StoryContent {
+    Properties(Properties),
+    Content(String),
+    Br,
+    #[serde(other)]
+    NotImplementedYet
+}
 
 #[derive(Default,Deserialize,Debug)]
 #[serde(rename_all="PascalCase")]
 pub struct Content {
     #[serde(rename="$value")]
-    text: Option<String>,
-    // br: Option<String>
+    text: Option<String>
 }
-
-// fn deserialize_content<'d, D: Deserializer<'d>>(d: D) -> Result<Option<String>, D::Error> {
-//     let Content { text, br } = Content::deserialize(d)?;
-//     Ok(text.or(br))
-// }
-
-// #[derive(Default,Deserialize,Debug)]
-// pub struct Br {
-//     text: String,
-// }
 
 pub fn parse_story_from_path(path: &Path) -> Result<StoryWrapper, DeError> {
     let xml = std::fs::read_to_string(path).unwrap();
-    
-    // let xml = r##"
-    // <idPkg:Story xmlns:idPkg="http://ns.adobe.com/AdobeInDesign/idml/1.0/packaging" DOMVersion="15.0">
-	// <Story Self="u373" AppliedTOCStyle="n" UserText="true" IsEndnoteStory="false" TrackChanges="false" StoryTitle="$ID/" AppliedNamedGrid="n">
-	// 	<ParagraphStyleRange AppliedParagraphStyle="ParagraphStyle/$ID/NormalParagraphStyle" Justification="CenterAlign">
-    //         <CharacterStyleRange>
-    //             <Properties>
-    //                 <SomeProperty>value</SomeProperty>
-    //             </Properties>
-    //             <Content>First line</Content>
-    //             <Br />
-    //             <Content>Second line</Content>
-    //             <Br />
-    //             <Content>Third line</Content>
-    //         </CharacterStyleRange>
-    //     </ParagraphStyleRange>
-    // </Story>
-    // </idPkg:Story>
-    // "##.to_string();
     
     // serde_xml_rs::from_str(xml.as_str())
     from_str(xml.as_str())
