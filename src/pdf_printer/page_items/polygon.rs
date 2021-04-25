26d57856b8e267000fa0c3854a9085c3148ab861
askepen <aske@vhg26.dk>
2021-04-25 23:03:32 +0200

    Cleaned parsing points in polygons

--- src/pdf_printer/page_items/polygon.rs
+++ src/pdf_printer/page_items/polygon.rs
@@ -3,8 +3,6 @@ use crate::idml_parser::IDMLResources;
 // use crate::pdf_printer::pdf_utils;
 use crate::pdf_printer::color_manager::{Color};
 use crate::pdf_printer::transforms::{self, Transform};
-// use printpdf::indices::{PdfLayerIndex, PdfPageIndex};
-// use printpdf::{Line, HPDF_Doc, Point, Pt};
 use libharu_sys::*;
 
 pub trait IsPolygon {
@@ -121,48 +119,24 @@ impl<T: IsPolygon> RenderPolygon for T {
         let item_transform = transforms::from_vec(self.get_item_transform());
 
         // Parse the points and apply the relevant transformations
-        let mut points: Vec<HPDF_REAL> = self
+        let mut points: Vec<Vec<HPDF_REAL>> = self
             .get_properties()
             .into_iter()
             .filter_map(|properties| properties.path_geometry().as_ref())
             .map(|path_geom| path_geom.geometry_path_type().path_point_arrays())
             .flat_map(|path_point_arrays| {
                 path_point_arrays.into_iter().flat_map(|path_point_array| {
-                    path_point_array
-                        .path_point_array()
-                        .into_iter()
-                        .map(|path_point_type| {
-                            [
-                                // Get anchor point and its two handles for the beizer curve
-                                path_point_type.anchor().as_ref(),
-                                path_point_type.left_direction().as_ref(),
-                                path_point_type.right_direction().as_ref(),
-                            ]
-                        })
-                        .filter(|[a, l, r]| a.is_some() && l.is_some() && r.is_some())
-                        .map(|[a, l, r]| [a.unwrap(), l.unwrap(), r.unwrap()])
-                        .map(|[a, l, r]| {
-                            [
-                                // Apply item and parent transformation matrices
-                                item_transform
-                                    .combine_with(&parent_transform)
-                                    .apply_to_point(&a[0], &a[1]),
-                                item_transform
-                                    .combine_with(&parent_transform)
-                                    .apply_to_point(&l[0], &l[1]),
-                                item_transform
-                                    .combine_with(&parent_transform)
-                                    .apply_to_point(&r[0], &r[1]),
-                            ]
-                        })
-                        .flat_map(|[a, l, r]| {
-                            vec![
-                                l[0] as f32, l[1] as f32, // Left handle
-                                a[0] as f32, a[1] as f32, // Anchor
-                                r[0] as f32, r[1] as f32, // Right handle
-                            ]
-                            .into_iter()
-                        })
+                    path_point_array.path_point_array().into_iter().flat_map(|path_point_type| {
+                        vec!(
+                            // Get anchor point and its two handles for the beizer curve
+                            path_point_type.left_direction().as_ref(),
+                            path_point_type.anchor().as_ref(),
+                            path_point_type.right_direction().as_ref(),
+                        ).into_iter()
+                    })
+                    .filter_map(|point| point)
+                    .map(|point| item_transform.combine_with(&parent_transform).apply_to_point(&point[0], &point[1]))
+                    .map(|point| point.iter().map(|&a| a as f32).collect())
                 })
             })
             .collect();
@@ -244,22 +218,17 @@ impl<T: IsPolygon> RenderPolygon for T {
             
             // The PDF library wants the points in a slightly different order
             // We just need to rotate the vec twice
-            points.rotate_right(2);
-            // points.append(&mut vec!(points[0], points[1]));
-
-            println!("");
+            points.rotate_right(1);
 
             // Start drawing from first anchor point
-            if points.len() >= 2 {
-                let l = points.len();
-                HPDF_Page_MoveTo(current_page, points[l-2], points[l-1]);
+            if let Some(last) = points.last() {
+                HPDF_Page_MoveTo(current_page, last[0], last[1]);
             }
             
-            // Loop over anchor points and bezier handles 
-            for slice in points.chunks(6) {
-                println!("{:?}", slice);
-                if let &[rx, ry, lx, ly, ax, ay] = slice {
-                    HPDF_Page_CurveTo(current_page, rx, ry, lx, ly, ax, ay);
+            // Draw the rest of the bezier curves  
+            for slice in points.chunks(3) {
+                if let [r, l, a] = slice {
+                    HPDF_Page_CurveTo(current_page, r[0], r[1], l[0], l[1], a[0], a[1]);
                 }
             }
 
